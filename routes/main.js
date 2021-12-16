253c3a8f4123ae4117defeae963510c01e29bded
abilash <e18013@eng.pdn.ac.lk>
2021-12-16 07:54:34 +0530

    json file connected

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,13 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/',(req,res,next)=>{
-    res.render('home')
+   
+    res.render('home',req.context)
+})
+
+router.get('/blog',(req,res,next)=>{
+
+    res.render('blog',req.context)
 })
 
 module.exports=router
